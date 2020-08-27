c11300c33e5d7519de646c4d46a746572e50bb3b
ZhongheSam <samdjk3680600@gmail.com>
2020-08-27 15:12:46 +0800

    blog update 2020/08/27

--- blog_project/blog/article/admin.py
+++ blog_project/blog/article/admin.py
@@ -3,7 +3,11 @@ from article.models import Article, Comment
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['article', 'content']
+    list_display = ['article', 'content', 'pubDateTime']
+    list_display_links = ['article']
+    list_filter = ['article', 'content']
+    search_fields = ['content']
+    list_editable = ['content']
 
     class Meta:
         model = Comment
