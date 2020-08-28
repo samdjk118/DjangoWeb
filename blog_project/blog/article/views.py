ec14782287d4b0ed7c6e96691d6fd69396a0d6c4
ZhongheSam <samdjk3680600@gmail.com>
2020-08-28 20:51:31 +0800

    articlePage finished 2020/08/28

--- blog_project/blog/article/views.py
+++ blog_project/blog/article/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from article.models import Article, Comment
+
 
 # Create your views here.
+
+
 def article(request):
-    return render(request,'article/article.html')
+    articles = {article: Comment.objects.filter(
+        article=article) for article in Article.objects.all()}
+    context = {'articles': articles}
+    return render(request, 'article/article.html', context)
